9f74bce2dac60f71f4b2ce56920bb9965ab9d5dd
looSeeCan <lly@icstars.org>
2024-08-29 16:52:48 -0500

    ....

--- app.mjs
+++ app.mjs
@@ -6,8 +6,6 @@ const app = express();
 app.use(express.static(path.join(process.cwd(), "src", "public")));
 
 app.get("/", (req, res) => {
-  // res.sendFile("index.html");
-  // res.sendfile(path.join(__dirname, "index.html"));
   res.sendFile(`${process.cwd()}/src/public/index.html`);
 });
 
